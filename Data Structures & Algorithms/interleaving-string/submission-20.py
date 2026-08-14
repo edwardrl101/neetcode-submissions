43178a17ce90a1b69e9f99f2c470d9f7fbb44574
edwardrl101 <142203135+edwardrl101@users.noreply.github.com>
2026-08-14 11:27:16 +0800

    Add: interleaving-string - submission-20

---
+++ Data Structures & Algorithms/interleaving-string/submission-20.py
@@ -0,0 +1,31 @@
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        if len(s1) + len(s2) != len(s3):
+            return False
+
+        m, n = len(s1), len(s2)
+
+        # dp[j] = dp[i+1][j]
+        dp = [False] * (n + 1)
+        dp[n] = True
+
+        for j in range(n - 1, -1, -1):
+            dp[j] = s2[j] == s3[m + j] and dp[j + 1]
+
+        for i in range(m - 1, -1, -1):
+            dp[n] = s1[i] == s3[i + n] and dp[n]
+
+            for j in range(n - 1, -1, -1):
+                take_s1 = (
+                    s1[i] == s3[i + j]
+                    and dp[j]
+                )
+
+                take_s2 = (
+                    s2[j] == s3[i + j]
+                    and dp[j + 1]
+                )
+
+                dp[j] = take_s1 or take_s2
+
+        return dp[0]
