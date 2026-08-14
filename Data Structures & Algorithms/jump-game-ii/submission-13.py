f6604c3ec0fa9a65d1cc230dbcd5ab7ac808f963
edwardrl101 <142203135+edwardrl101@users.noreply.github.com>
2026-08-14 17:09:59 +0800

    Add: jump-game-ii - submission-13

---
+++ Data Structures & Algorithms/jump-game-ii/submission-13.py
@@ -0,0 +1,16 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        cur = 0
+        cnt = 0
+        n = len(nums)
+        while cur < n-1:
+            if cur + nums[cur] >= n-1:
+                return cnt+1
+            mx, mxi = 0, cur+1
+            for k in range(cur+1, min(n, cur+nums[cur]+1)):
+                if k+nums[k] >= mx:
+                    mx = k+nums[k]
+                    mxi = k
+            cur = mxi
+            cnt += 1
+        return 0
